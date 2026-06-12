4a27aa2405c89b42993312d1d27c48085153738c
Zheng Hanbin <hanbinzheng54@gmail.com>
2026-06-12 22:25:24 +0800

    feat(spi): add spi driver without thorough test

---
+++ bsp/spi/bsp_spi.c
@@ -0,0 +1,119 @@
+#include "bsp_spi.h"
+#include "stm32h7xx_hal_def.h"
+#include "stm32h7xx_hal_spi.h"
+#include <assert.h>
+#include <sys/_types.h>
+
+#define SPI_TIMEOUT_MS 100
+
+struct spi_inst {
+	SPI_HandleTypeDef *handle;
+	GPIO_TypeDef *cs_port;
+	uint16_t cs_pin;
+
+	/* enum spi_work_mode work_mode; */
+};
+
+static struct spi_inst spi_inst[SPI_INST_MAX_NUM];
+static uint8_t idx = 0;
+
+struct spi_inst *spi_register(const struct spi_config *config)
+{
+	assert(config != NULL && config->handle != NULL && idx < SPI_INST_MAX_NUM);
+
+	/* check repetition */
+	for (uint8_t i = 0; i < idx; i++) {
+		if (spi_inst[i].handle == config->handle) {
+			/* in our case, 2 different handler */
+			return (spi_inst + i);
+		}
+	}
+
+	struct spi_inst *inst = (spi_inst + idx);
+	inst->handle = config->handle;
+	inst->cs_pin = config->cs_pin;
+	inst->cs_port = config->cs_port;
+	HAL_GPIO_WritePin(inst->cs_port, inst->cs_pin, GPIO_PIN_SET);
+	idx++;
+
+	return inst;
+}
+
+/* reserved
+void spi_select(struct spi_inst *inst)
+{
+	assert(inst != NULL);
+	HAL_GPIO_WritePin(inst->cs_port, inst->cs_pin, GPIO_PIN_RESET);
+}
+
+void spi_deselect(struct spi_inst *inst)
+{
+	assert(inst != NULL);
+	HAL_GPIO_WritePin(inst->cs_port, inst->cs_pin, GPIO_PIN_SET);
+}
+*/
+
+HAL_StatusTypeDef spi_transmit(struct spi_inst *inst, uint8_t *buff, uint16_t len)
+{
+	assert(inst != NULL && buff != NULL && inst->handle != NULL);
+
+	/* check stm32h7xx_hal_gpio.h/c for GPIO_TypeDef and HAL_GPIO_WritePin() */
+	if (inst->cs_port == NULL || inst->cs_pin == 0) { /* GPIO_PIN_0 is (uint16_t)1 */
+		return HAL_SPI_Transmit(inst->handle, buff, len, SPI_TIMEOUT_MS);
+	}
+
+	HAL_StatusTypeDef ret = HAL_ERROR; /* returen value */
+	HAL_GPIO_WritePin(inst->cs_port, inst->cs_pin, GPIO_PIN_RESET);
+	ret = HAL_SPI_Transmit(inst->handle, buff, len, SPI_TIMEOUT_MS);
+	HAL_GPIO_WritePin(inst->cs_port, inst->cs_pin, GPIO_PIN_SET);
+	return ret;
+
+	/* reserved modes: need manually select CS but not manually deselect CS */
+	/* HAL_SPI_TransmitReceive_IT(inst->handle, tx_buff, rx_buff, len); */
+	/* HAL_SPI_TransmitReceive_DMA(inst->handle, tx_buff, rx_buff, len); */
+}
+
+HAL_StatusTypeDef spi_receive(struct spi_inst *inst, uint8_t *buff, uint16_t len)
+{
+	assert(inst != NULL && buff != NULL && inst->handle != NULL);
+
+	/* check stm32h7xx_hal_gpio.h/c for GPIO_TypeDef and HAL_GPIO_WritePin() */
+	if (inst->cs_port == NULL || inst->cs_pin == 0) { /* GPIO_PIN_0 is (uint16_t)1 */
+		return HAL_SPI_Receive(inst->handle, buff, len, SPI_TIMEOUT_MS);
+	}
+
+	HAL_StatusTypeDef ret = HAL_ERROR; /* returen value */
+	HAL_GPIO_WritePin(inst->cs_port, inst->cs_pin, GPIO_PIN_RESET);
+	ret = HAL_SPI_Receive(inst->handle, buff, len, SPI_TIMEOUT_MS);
+	HAL_GPIO_WritePin(inst->cs_port, inst->cs_pin, GPIO_PIN_SET);
+	return ret;
+
+	/* reserved modes: need manually select CS but not manually deselect CS */
+	/* HAL_SPI_TransmitReceive_IT(inst->handle, tx_buff, rx_buff, len); */
+	/* HAL_SPI_TransmitReceive_DMA(inst->handle, tx_buff, rx_buff, len); */
+}
+
+HAL_StatusTypeDef spi_transceive(struct spi_inst *inst, uint8_t *tx_buff, uint8_t *rx_buff,
+				 uint16_t len)
+{
+	assert(inst != NULL && tx_buff != NULL && rx_buff != NULL && inst->handle != NULL);
+
+	/* check stm32h7xx_hal_gpio.h/c for GPIO_TypeDef and HAL_GPIO_WritePin() */
+	if (inst->cs_port == NULL || inst->cs_pin == 0) { /* GPIO_PIN_0 is (uint16_t)1 */
+		return HAL_SPI_TransmitReceive(inst->handle, tx_buff, rx_buff, len, SPI_TIMEOUT_MS);
+	}
+
+	HAL_StatusTypeDef ret = HAL_ERROR; /* returen value */
+	HAL_GPIO_WritePin(inst->cs_port, inst->cs_pin, GPIO_PIN_RESET);
+	ret = HAL_SPI_TransmitReceive(inst->handle, tx_buff, rx_buff, len, SPI_TIMEOUT_MS);
+	HAL_GPIO_WritePin(inst->cs_port, inst->cs_pin, GPIO_PIN_SET);
+	return ret;
+
+	/* reserved modes: need manually select CS but not manually deselect CS */
+	/* HAL_SPI_TransmitReceive_IT(inst->handle, tx_buff, rx_buff, len); */
+	/* HAL_SPI_TransmitReceive_DMA(inst->handle, tx_buff, rx_buff, len); */
+}
+
+/* reserved callback functions */
+/* void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef *hspi) */
+/* void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef *hspi) */
---
+++ bsp/spi/bsp_spi.h
@@ -0,0 +1,101 @@
+#ifndef BSP_SPI_H_
+#define BSP_SPI_H_
+
+#include "gpio.h"
+#include "spi.h"
+#include "stm32h7xx_hal_def.h"
+#include <stdint.h>
+
+#define SPI_INST_MAX_NUM 2 /* only 2 in use */
+
+/* only supports simple transmit and receive under block mode now */
+
+struct spi_inst;
+
+/* reserved: SPI transmit/receive mode enum
+enum spi_work_mode {
+	SPI_BLOCK_MODE = 0,
+	SPI_IT_MODE,
+	SPI_DMA_MODE,
+};
+*/
+
+struct spi_config {
+	SPI_HandleTypeDef *handle; /* hspi1, hspi2... */
+	GPIO_TypeDef *cs_port;	   /* GPIOA, GPIOB... */
+	uint16_t cs_pin;	   /* GPIO_PIN_1, GPIO_PIN_2... */
+
+	/* enum spi_work_mode work_mode; */
+};
+
+/**
+ * @brief register a SPI instance
+ *
+ * @note this should be called after the initialization of all peripherals.
+ * specially, all MX_SPIX_Init() functions.
+ * config->handle must be valid, and by default, the CS is not selected.
+ *
+ * @param config SPI instance configuration struct
+ * @return pointer to the SPI instance
+ *
+ */
+struct spi_inst *spi_register(const struct spi_config *config);
+
+/**
+ * @brief select the CS
+ *
+ * @param inst pointer to the SPI instance
+ */
+/* void spi_select(struct spi_inst *inst); */
+
+/**
+ * @brief release the CS
+ *
+ * @param inst pointer to the SPI instance
+ */
+/* void spi_deselect(struct spi_inst *inst); */
+
+/**
+ * @brief transmit in block mode
+ *
+ * blocking time: 100ms
+ * CS will be asserted before transmission and deasserted after.
+ * for invalid cs_port and cs_pin, CS operation is omitted
+ *
+ * @param inst pointer to the SPI instance
+ * @param buff transmit buffer
+ * @param len number of bytes to transmit in tx buffer
+ * @return whether successful
+ */
+HAL_StatusTypeDef spi_transmit(struct spi_inst *inst, uint8_t *buff, uint16_t len);
+
+/**
+ * @brief receive data in blocking mode
+ *
+ * blocking timeout: 100ms
+ * CS will be asserted before reception and deasserted after.
+ * for invalid cs_port and cs_pin, CS operation is omitted.
+ *
+ * @param inst pointer to the SPI instance
+ * @param buff receive buffer
+ * @param len number of bytes to receive
+ * @return whether successful
+ */
+HAL_StatusTypeDef spi_receive(struct spi_inst *inst, uint8_t *buff, uint16_t len);
+
+/**
+ * @brief transmit and receive simultaneously in blocking mode
+ *
+ * blocking timeout: 100ms
+ * CS will be asserted before transaction and deasserted after.
+ *
+ * @param inst pointer to the SPI instance
+ * @param tx_buff tx buffer (data to send)
+ * @param rx_buff rx buffer (received data)
+ * @param len number of bytes to transmit/receive
+ * @return whether successful
+ */
+HAL_StatusTypeDef spi_transceive(struct spi_inst *inst, uint8_t *tx_buff, uint8_t *rx_buff,
+				 uint16_t len);
+
+#endif /* BSP_SPI_H_ */
