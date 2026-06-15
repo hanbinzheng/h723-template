cca48fbb45055739d8acc91022e0968723a1d72b
Zheng Hanbin <hanbinzheng54@gmail.com>
2026-06-16 02:50:20 +0800

    feat(ws2812): add NULL assertion

--- device/ws2812/ws2812.c
+++ device/ws2812/ws2812.c
@@ -1,18 +1,22 @@
 #include "ws2812.h"
 #include "bsp_spi.h"
+#include <assert.h>
 
 #define WS2812_LOW_LEVEL 0xC0
 #define WS2812_HIGH_LEVEL 0xF0
 
-static struct spi_inst *ws2812_spi;
+static struct spi_inst *ws2812_spi = NULL;
 
 void ws2812_register(const struct spi_inst *spi)
 {
+	assert(spi != NULL);
 	ws2812_spi = (struct spi_inst *)spi;
 }
 
 void ws2812_ctrl(uint8_t r, uint8_t g, uint8_t b)
 {
+	assert(ws2812_spi != NULL);
+
 	uint8_t tx_buff[24];
 	uint8_t res = 0;
 	for (int i = 0; i < 8; i++) {
@@ -20,6 +24,7 @@ void ws2812_ctrl(uint8_t r, uint8_t g, uint8_t b)
 		tx_buff[15 - i] = (((r >> i) & 0x01) ? WS2812_HIGH_LEVEL : WS2812_LOW_LEVEL) >> 1;
 		tx_buff[23 - i] = (((b >> i) & 0x01) ? WS2812_HIGH_LEVEL : WS2812_LOW_LEVEL) >> 1;
 	}
+
 	spi_transmit(ws2812_spi, tx_buff, 24);
 	for (int i = 0; i < 100; i++) {
 		spi_transmit(ws2812_spi, &res, 1);
