e6a4dafde3f4264daee7e7ae26e6d5687e3df2ac
Zheng Hanbin <hanbinzheng54@gmail.com>
2026-06-17 20:56:28 +0800

    feat(tim): add basic driver for tim with pwm

---
+++ bsp/tim/bsp_tim.c
@@ -0,0 +1,81 @@
+#include "bsp_tim.h"
+#include <assert.h>
+
+struct tim_inst {
+	TIM_HandleTypeDef *htim;
+	enum tim_mode mode;
+	uint32_t clk_freq;
+	uint32_t channel;
+};
+
+static struct tim_inst tim_inst[TIM_INST_MAX_NUM];
+static uint8_t idx = 0;
+
+struct tim_inst *tim_register(const struct tim_config *config)
+{
+	assert(config != NULL && config->htim != NULL && idx < TIM_INST_MAX_NUM);
+
+	/* check repetition */
+	for (uint8_t i = 0; i < idx; i++) {
+		struct tim_inst *tmp = (tim_inst + i);
+		if (tmp->htim == config->htim && tmp->channel == config->channel) {
+			return tmp;
+		}
+	}
+
+	struct tim_inst *inst = (tim_inst + idx);
+	inst->htim = config->htim;
+	inst->mode = config->mode;
+	inst->clk_freq = config->clk_freq;
+	inst->channel = config->channel;
+
+	switch (inst->mode) {
+	case TIM_INTERRUPT_MODE:
+		if (HAL_TIM_Base_Start_IT(inst->htim) != HAL_OK) {
+			return NULL;
+		}
+		break;
+	case TIM_PWM_MODE:
+		if (HAL_TIM_Base_Start(inst->htim) != HAL_OK) {
+			return NULL;
+		}
+		break;
+	};
+	idx++;
+
+	return inst;
+}
+
+HAL_StatusTypeDef tim_pwm_start(const struct tim_inst *inst)
+{
+	assert(inst != NULL);
+	return HAL_TIM_PWM_Start(inst->htim, inst->channel);
+}
+
+HAL_StatusTypeDef tim_pwm_stop(const struct tim_inst *inst)
+{
+	assert(inst != NULL);
+	return HAL_TIM_PWM_Stop(inst->htim, inst->channel);
+}
+
+/* check stm32h7xx_hal_tim.h for details */
+HAL_StatusTypeDef tim_set_pwm(struct tim_inst *inst, uint16_t pwm_freq, float pwm_duty)
+{
+	assert(inst != NULL && inst->htim != NULL);
+	TIM_HandleTypeDef *htim = inst->htim;
+	uint32_t pwm_freq_max = inst->clk_freq / (htim->Init.Prescaler + 1);
+	if (inst->mode != TIM_PWM_MODE || pwm_freq > pwm_freq_max) {
+		return HAL_ERROR;
+	}
+
+	/* change htim->Init.Period and the auto-loader register htim->Instance->ARR */
+	uint16_t arr = (uint16_t)((float)pwm_freq_max / (float)pwm_freq - 1.0f);
+	__HAL_TIM_SET_AUTORELOAD(htim, arr);
+
+	/* change pwm pusle, the capture & compare register CCR */
+	pwm_duty = pwm_duty > 1.0f ? 1.0f : pwm_duty;
+	pwm_duty = pwm_duty < 0.0f ? 0.0f : pwm_duty;
+	__HAL_TIM_SET_COMPARE(htim, inst->channel, (uint16_t)(arr * pwm_duty));
+
+	return HAL_OK;
+}
---
+++ bsp/tim/bsp_tim.h
@@ -0,0 +1,57 @@
+#ifndef BSP_TIM_
+#define BSP_TIM_
+
+#include "tim.h"
+
+#define TIM_INST_MAX_NUM 5
+
+struct tim_inst;
+
+enum tim_mode {
+	TIM_INTERRUPT_MODE = 0,
+	TIM_PWM_MODE,
+};
+
+struct tim_config {
+	TIM_HandleTypeDef *htim;
+	uint32_t channel; /* TIM_CHANNEL_1, TIM_CHANNEL_2... */
+	enum tim_mode mode;
+	uint32_t clk_freq;
+};
+
+/**
+ * @brief register a TIM instance
+ *
+ * @note this should be called after the initialization of all peripherals.
+ * specially, all MX_TIMX_Init() functions.
+ *
+ * @attention for PWM, you should manually open it by tim_pwm_start()
+ *
+ * @param config TIM instance configuration struct
+ * @return pointer to the TIM instance
+ *
+ */
+struct tim_inst *tim_register(const struct tim_config *config);
+
+/**
+ * @brief control on and off of PWM
+ *
+ * @param inst pointer to tim_inst
+ * @return whether this operation is successful
+ */
+HAL_StatusTypeDef tim_pwm_start(const struct tim_inst *inst);
+HAL_StatusTypeDef tim_pwm_stop(const struct tim_inst *inst);
+
+/**
+ * @brief change the setting of pwm in running time
+ *
+ * @note users should make sure you
+ *
+ * @param inst pointer to tim_inst
+ * @param pwm_freq target pwm frequency
+ * @param pwm_duty target pwm duty, in range [0, 1]
+ * @return whether the modification is successful
+ */
+HAL_StatusTypeDef tim_set_pwm(struct tim_inst *inst, uint16_t pwm_freq, float pwm_duty);
+
+#endif /* BSP_TIM_ */
