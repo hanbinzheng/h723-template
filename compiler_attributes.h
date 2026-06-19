f5b1c0d1874dcac8ba6a70398448fd3a47694348
Zheng Hanbin <hanbinzheng54@gmail.com>
2026-06-20 00:49:01 +0800

    feat: add ITCM support for time-critical functions
    - Add compiler_attributes.h with __ITCM_FUNC macro for placing functions into ITCMRAM section
    - Update linker script to define .itcm_text section
    - Modify startup code to copy .itcm_text from FLASH to ITCMRAM during boot
    - Add -include compiler_attributes.h in CMakeLists.txt for global availability
    
    This allows critical functions to run from zero-wait ITCMRAM to improve performance

---
+++ compiler_attributes.h
@@ -0,0 +1,14 @@
+#ifndef COMPILER_ATTRIBUTES_H_
+#define COMPILER_ATTRIBUTES_H_
+
+#ifdef __GNUC__
+#define __ITCM_FUNC __attribute__((section(".itcm_text")))
+#define __ALWAYS_INLINE __attribute__((always_inline)) inline
+#define __WEAK __attribute__((weak))
+#else
+#define __ITCM_FUNC
+#define __ALWAYS_INLINE inline
+#define __WEAK
+#endif
+
+#endif /* COMPILER_ATTRIBUTES_H_ */
--- startup_stm32h723xx.s
+++ startup_stm32h723xx.s
@@ -39,6 +39,14 @@ defined in linker script */
 .word  _sdata
 /* end address for the .data section. defined in linker script */
 .word  _edata
+
+/* start address of corresponding sections in FLASH (LMA) */
+.word  _siitcm_text
+/* start of ITCMRAM run time address (VMA) */
+.word  _sitcm_text
+/* end of ITCMRAM run time address */
+.word  _eitcm_text
+
 /* start address for the .bss section. defined in linker script */
 .word  _sbss
 /* end address for the .bss section. defined in linker script */
@@ -81,6 +89,24 @@ LoopCopyDataInit:
   adds r4, r0, r3
   cmp r4, r1
   bcc CopyDataInit
+
+/* Copy the code segments from FLASH to ITCMRAM */
+  ldr r0, =_sitcm_text
+  ldr r1, =_eitcm_text
+  ldr r2, =_siitcm_text
+  movs r3, #0
+  b LoopCopyItcmInit
+
+CopyItcmInit:
+  ldr r4, [r2, r3]
+  str r4, [r0, r3]
+  adds r3, r3, #4
+
+LoopCopyItcmInit:
+  adds r4, r0, r3
+  cmp r4, r1
+  bcc CopyItcmInit
+
 /* Zero fill the bss segment. */
   ldr r2, =_sbss
   ldr r4, =_ebss
